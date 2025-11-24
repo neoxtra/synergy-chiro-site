72f86486e424a22c134a26f978a03256f63e0ee9
Work <edmondtan1053@gmail.com>
2025-11-24 03:45:18 -0500

    quick title fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "NeoTek AI Assistant",
+  title: "NeoTek Designs",
   description: "Website with integrated AI front desk assistant.",
 };
 
